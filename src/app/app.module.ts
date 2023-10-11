32e3595a67b066eaa0238ae22824f84c9f00b729
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-10-11 23:17:02 +0545

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,64 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material-module';
+import { HomeComponent } from './home/home.component';
+import { BestSellerComponent } from './best-seller/best-seller.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { SharedModule } from './shared/shared.module';
+import { FullComponent } from './layouts/full/full.component';
+import { AppHeaderComponent } from './layouts/full/header/header.component';
+import { AppSidebarComponent } from './layouts/full/sidebar/sidebar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SignupComponent } from './signup/signup.component';
+import {NgxUiLoaderModule, NgxUiLoaderConfig,SPINNER,PB_DIRECTION} from "ngx-ui-loader"
+
+
+
+const ngxUiLoaderConfig:NgxUiLoaderConfig={
+  text:'Loading...',
+  textColor:'#ffffff',
+  textPosition:"center-center",
+  pbColor:"red",
+  bgsColor:"red",
+  fgsColor:"red",
+  fgsType:SPINNER.ballSpinClockwise,
+  fgsSize:100,
+  pbDirection:PB_DIRECTION.leftToRight,
+  pbThickness:5
+ }
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    HomeComponent,
+    BestSellerComponent,
+    FullComponent,
+    AppHeaderComponent,
+    AppSidebarComponent,
+    SignupComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    MatIconModule,
+    SharedModule,MatMenuModule ,
+    HttpClientModule,
+    MatDialogModule,
+    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
